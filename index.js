d59d45a9127e1ecd2c0d2275016e781c39e3197f
Patrick Schaller <pascha@MacBook-Pro.local>
2017-03-19 16:24:46 +0100

    first test

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Testfile to run on Minishift
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello MiniShift!');
+});
+
+app.listen(3000, function () {
+  console.log('Simple app listening on port 3000!');
+});
